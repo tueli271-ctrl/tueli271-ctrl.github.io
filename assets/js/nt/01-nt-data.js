460b1c09b3dc4208462967a0a7477fd032c3d79c
tueli271-ctrl <tueli271@gmail.com>
2025-12-28 02:50:09 +0700

    Create 01-nt-data.js

---
+++ assets/js/nt/01-nt-data.js
@@ -0,0 +1,30 @@
+// assets/js/nt/01-nt-data.js
+MathHub.NT.posts = [
+  {
+    id: "nt-01",
+    num: "01",
+    title: "Bài mẫu: Chia hết cơ bản",
+    difficulty: "N1",
+    topic: "Chia hết",
+    method: ["Đồng dư", "Ước - bội"],
+    href: "view.html?id=nt-01"
+  },
+  {
+    id: "nt-02",
+    num: "02",
+    title: "Bài mẫu: Đồng dư nâng dần",
+    difficulty: "N2",
+    topic: "Đồng dư",
+    method: ["Số học modulo"],
+    href: "view.html?id=nt-02"
+  },
+  {
+    id: "nt-03",
+    num: "03",
+    title: "Bài mẫu: LTE cơ bản",
+    difficulty: "N3",
+    topic: "Hàm số học",
+    method: ["LTE"],
+    href: "view.html?id=nt-03"
+  }
+];
