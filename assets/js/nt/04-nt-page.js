47652d62b82fb14b3a41d69a12ecc13273e2bcfc
tueli271-ctrl <tueli271@gmail.com>
2025-12-28 14:58:36 +0700

    Update 04-nt-page.js

--- assets/js/nt/04-nt-page.js
+++ assets/js/nt/04-nt-page.js
@@ -25,19 +25,34 @@ MathHub.NT.init = function initNT() {
 
   // 5) filter change
   if (panel) {
-    panel.addEventListener("change", () => {
-      MathHub.NT.readFilterState();
-      MathHub.NT.renderList();
-    });
-  }
+  panel.addEventListener("change", (e) => {
+    // SFX (nếu có)
+    if (window.MathHub && MathHub.SFX && typeof MathHub.SFX.play === "function") {
+      const t = e.target;
+      if (t && t.matches && t.matches('input[type="checkbox"]')) {
+        MathHub.SFX.play(t.checked ? "select" : "unselect");
+      } else {
+        MathHub.SFX.play("click");
+      }
+    }
+
+    MathHub.NT.readFilterState();
+    MathHub.NT.renderList();
+  });
+}
+
 
   // 6) clear filters
   const clearBtn = qs("#ntClearFilters");
   if (clearBtn && panel) {
     clearBtn.addEventListener("click", () => {
-      qsa('#ntTaxPanel input[type="checkbox"]').forEach(i => (i.checked = false));
-      MathHub.NT.readFilterState();
-      MathHub.NT.renderList();
-    });
+  qsa('#ntTaxPanel input[type="checkbox"]').forEach(i => (i.checked = false));
+
+  // SFX
+  if (window.MathHub && MathHub.SFX && typeof MathHub.SFX.play === "function") {
+    MathHub.SFX.play("clear");
   }
-};
+
+  MathHub.NT.readFilterState();
+  MathHub.NT.renderList();
+});
